db6ea054783ff2b0bb3ba8695150f92abc820c5a
PWashburn55 <pw325555@gmail.com>
2020-08-03 19:46:19 +0000

    adding requirements.txt and run.py

---
+++ run.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello There!</h1>"
+
+
+#if __name__ =="__main__":
+   # app.run(host=os.getenv("IP"),
+       #port=int(os.getenv("PORT")),
+       #debug=True)
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
+
