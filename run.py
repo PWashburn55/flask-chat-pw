5d43713ef894bd270a0aca0d847170af9d7040cc
PWashburn55 <pw325555@gmail.com>
2020-08-04 19:51:00 +0000

    factoring run.py

--- run.py
+++ run.py
@@ -52,7 +52,7 @@ def send_message(username, message):
        #port=int(os.getenv("PORT")),
        #debug=True)
 
-"""if __name__ == "__main__":"""
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
